1521390e210eadf2da2a3740d1e48e0975d8fcd6
azzamunza <azzamunza@gmail.com>
2025-10-16 00:58:51 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,6 @@ window.CRM_CONFIG = {
     GOOGLE_CLIENT_ID: '',  // Will be populated by GitHub Actions
     GITHUB_USERNAME: '',    // Will be populated by GitHub Actions
     GITHUB_REPO: '',        // Will be populated by GitHub Actions
-    GITHUB_TOKEN: '',       // Will be populated by GitHub Actions
     USERS_FILE: 'users.json',
     DATA_FILE: 'crm-data.json'
 };
