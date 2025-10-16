c3783f81dbc5288482bc47369dcfdfd504560619
azzamunza <azzamunza@users.noreply.github.com>
2025-10-16 06:44:04 +0000

    deploy: 32eb3609e04dd9674cc7f0f22ec27508a7062cbe

---
+++ config.js
@@ -0,0 +1,9 @@
+// Auto-generated config file - DO NOT EDIT MANUALLY
+window.CRM_CONFIG = {
+  GOOGLE_CLIENT_ID: '957511612141-hk0pp224v1cqe9sv5khq6u6b40plnnje.apps.googleusercontent.com',
+  GITHUB_USERNAME: 'azzamunza',
+  GITHUB_REPO: 'AmyCRM',
+  GITHUB_TOKEN: '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',
+  USERS_FILE: 'users.json',
+  DATA_FILE: 'crm-data.json'
+};
