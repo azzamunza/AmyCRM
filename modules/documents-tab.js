438c252e21a580c8b2ff76ac44217328f0375b69
azzamunza <azzamunza@gmail.com>
2025-10-19 12:03:27 +0800

    Update documents-tab.js

--- modules/documents-tab.js
+++ modules/documents-tab.js
@@ -319,7 +319,7 @@ const DocumentsTab = {
         alert(message);
     }
 };
-window.ContactsTab = DocumentsTab;
+window.DocumentsTab = DocumentsTab;
 
 // Auto-initialize when tab is shown
 // This will be called from dashboard.html when the tab is clicked
